e3a17628a78af540a77ac05e07d74a1ae3231503
suey0128 <syui0128@gmail.com>
2021-10-01 16:29:40 -0700

    added about section

--- shuyiyu.com/src/components/About/AboutElements.js
+++ shuyiyu.com/src/components/About/AboutElements.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const AboutContainer = styled.div`
     background: #253f36;
-    diplay:flex;
+    display: flex;
     justify-content: center;
     align-items: center;
     padding: 0 30px;
@@ -30,4 +30,60 @@ export const VideoBg = styled.video`
     -o-object-fit: cover;
     object-fit: cover;
     background: #253f36;
+`
+
+export const AboutContent = styled.div`
+    z-index: 3;
+    max-width: 1200px;
+    position absolute;
+    padding: 8px 24px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-item: center;
+`
+export const AboutH2 = styled.h2`
+    color: #ff7d5b;
+    font-size:36px;
+    text-align: center;
+
+    @media screen and (max-width: 768px) {
+        font-size: 28px;
+    }
+
+    @media screen and (max-width: 480px) {
+        font-size: 24px;
+    }
+`
+
+export const AboutH1 = styled.h1`
+    margin-top: 12px;
+    color: #ff7d5b;
+    font-size:48px;
+    text-align: center;
+
+    @media screen and (max-width: 768px) {
+        font-size: 40px;
+    }
+
+    @media screen and (max-width: 480px) {
+        font-size: 32px;
+    }
+`
+
+
+export const AboutP = styled.p`
+    color: #ff7d5b;
+    margin-top: 24px;
+    font-size:24px;
+    text-align: center;
+    max-width: 600px;
+
+    @media screen and (max-width: 768px) {
+        font-size: 24px;
+    }
+
+    @media screen and (max-width: 480px) {
+        font-size: 18px;
+    }
 `
