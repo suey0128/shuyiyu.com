fe5dc05ebe6067bde92c5a2bd384effbed5d1eb6
suey0128 <syui0128@gmail.com>
2021-10-17 22:02:27 -0700

    added icon and changed website name

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect  } from 'react';
 import About from '../components/About';
 import Navbar from '../components/Navbar';
 import Sidebar from '../components/Sidebar';
